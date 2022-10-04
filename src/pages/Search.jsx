c39faf5c85b5ad860dd088b86472918c8b42ce28
Matheus <matheusaraujo.lopes02092016@gmail.com>
2022-10-04 17:08:00 -0300

    requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,50 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    isDisabled: true,
+    input: '',
+  };
+
+  handleChange = ({ target }) => {
+    this.setState({
+      input: target.value,
+    }, () => {
+      const { input } = this.state;
+      const limit = 2;
+      this.setState({
+        isDisabled: input.length < limit });
+    });
+  };
+
   render() {
+    const { isDisabled, input } = this.state;
     return (
-      <div data-testid="page-search">
-        <Header />
-      </div>
+      <>
+        <div data-testid="page-search">
+          <Header />
+        </div>
+        <form action="">
+          <label htmlFor="name">
+            <input
+              type="text"
+              name="input"
+              id="name"
+              data-testid="search-artist-input"
+              placeholder="Artists, songs, or albums"
+              onChange={ this.handleChange }
+              value={ input }
+            />
+          </label>
+          <button
+            data-testid="search-artist-button"
+            type="button"
+            disabled={ isDisabled }
+          >
+            Pesquisar
+          </button>
+        </form>
+      </>
     );
   }
 }
