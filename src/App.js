12138d1a474974f323bfe4d477faaaeac6664a0f
Matheus <matheusaraujo.lopes02092016@gmail.com>
2022-10-04 22:40:57 -0300

    meu melhor no dia de hoje para a 6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
 import Login from './pages/Login';
@@ -13,15 +13,15 @@ class App extends React.Component {
     return (
       <>
         <p>TrybeTunes!</p>
-        <main>
+        <Switch>
           <Route exact path="/" component={ Login } />
           <Route exact path="/search" component={ Search } />
           <Route exact path="/album/:id" component={ Album } />
           <Route exact path="/favorites" component={ Favorites } />
           <Route exact path="/profile" component={ Profile } />
           <Route exact path="/profile/edit" component={ ProfileEdit } />
-          <Route exact path="" component={ NotFound } />
-        </main>
+          <Route path="*" component={ NotFound } />
+        </Switch>
       </>
     );
   }
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,50 +1,88 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from '../components/Loading';
 
 class Search extends React.Component {
-  state = {
-    isDisabled: true,
-    input: '',
-  };
+  constructor() {
+    super();
+    this.state = {
+      isDisabled: true,
+      text: '',
+      loading: false,
+      artist: '',
+      data: [],
+      showList: false,
+    };
+  }
 
   handleChange = ({ target }) => {
     this.setState({
-      input: target.value,
+      text: target.value,
     }, () => {
-      const { input } = this.state;
+      const { text } = this.state;
       const limit = 2;
       this.setState({
-        isDisabled: input.length < limit });
+        isDisabled: text.length < limit });
     });
   };
 
+  handleClick = async () => {
+    const { text } = this.state;
+    this.setState({ loading: true });
+    const list = await searchAlbumsAPI(text);
+    this.setState({ loading: false, text: '', data: list, showList: true });
+  };
+
   render() {
-    const { isDisabled, input } = this.state;
+    const { isDisabled, text, loading, data, showList, artist } = this.state;
+    if (loading) return <Loading />;
     return (
       <>
         <div data-testid="page-search">
           <Header />
-        </div>
-        <form action="">
+          <h1>Search</h1>
           <label htmlFor="name">
             <input
               type="text"
-              name="input"
+              name="text"
               id="name"
               data-testid="search-artist-input"
               placeholder="Artists, songs, or albums"
               onChange={ this.handleChange }
-              value={ input }
+              value={ text }
             />
           </label>
           <button
             data-testid="search-artist-button"
             type="button"
             disabled={ isDisabled }
+            onClick={ this.handleClick }
           >
             Pesquisar
           </button>
-        </form>
+        </div>
+        { showList && (
+          <h2>
+            {`Resultado de álbum de: ${artist}`}
+          </h2>
+        ) }
+        { data.length === 0 ? <p>Nenhum álbum foi encontrado</p> : (
+          data.map((song) => (
+            <div key={ song.collectionName }>
+              <img src={ song.artworkUrl100 } alt={ song.collectionName } />
+              <p>{ song.collectionName }</p>
+              <p>{ song.artistName }</p>
+              <Link
+                to={ `/album/${song.collectionId}` }
+                data-testid={ `link-to-album-${collectionId}` }
+              >
+                Ver álbum
+              </Link>
+            </div>
+          ))
+        )}
       </>
     );
   }
